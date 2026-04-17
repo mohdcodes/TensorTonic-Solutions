866717cc78cf8650a184505567cb62e907ed18c3
Mohd Arbaaz Siddiqui <112058693+mohdcodes@users.noreply.github.com>
2026-04-17 15:24:28 +0530

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -4,5 +4,6 @@ def sigmoid(x):
     """
     Vectorized sigmoid function.
     """
+    # created numpy array because it will be implementing the functions to each element. So no need to worry about the type of x
     x = np.array(x, dtype = float)
     return 1/(1+np.exp(-x))
